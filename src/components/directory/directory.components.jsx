112c86c051a63cf783f12d8e1f2faefd3f4756bb
svladiko <svladiko@mail.ru>
2021-05-13 18:19:03 +0300

    Added components and pages folders. Added components: directory, menu-item,

---
+++ src/components/directory/directory.components.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import MenuItem from "../menu-item/menu-item.components";
+
+import sections from './directory.data';
+import './directory.styles.scss';
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            sections: sections,
+        }
+    }
+
+    render() {
+        console.log(';', this.state.sections);
+
+        return (
+            <div className='directory-menu'>
+                {
+                    this.state.sections.map(({title, imageUrl, id,size}) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default Directory;
