eb16fa99e2d0d09b6b40bd478ca5138ffc1b497e
svladiko <svladiko@mail.ru>
2021-05-14 13:41:41 +0300

    Added router navigation for MenuItem

--- src/components/directory/directory.components.jsx
+++ src/components/directory/directory.components.jsx
@@ -18,8 +18,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title, imageUrl, id,size}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    this.state.sections.map(({id, ...otherSelectionProps}) => (
+                        <MenuItem key={id} {...otherSelectionProps} />
                     ))
                 }
             </div>
