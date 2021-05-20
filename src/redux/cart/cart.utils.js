d91354a392f8fa69efd4a4c4b67fc5ebbf963448
svladiko <svladiko@mail.ru>
2021-05-20 19:29:28 +0300

    Added increase, decrease cart item quantity for checkout-item.component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,4 +1,4 @@
- export const addItemToCart = (cartItems, cartItemToAdd) => {
+export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
 
     if (existingCartItem) {
@@ -10,6 +10,17 @@
     }
 
     return [...cartItems, {...cartItemToAdd, quantity: 1}]
- }
+}
 
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id);
 
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+
+    return cartItems.map(cartItem => cartItem.id === cartItemToRemove.id
+        ? {...cartItem, quantity: cartItem.quantity - 1}
+        : cartItem
+    )
+}
